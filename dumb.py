6a6e3fa9872d55a90eac54c31374ead48ba98e9b
Chris <christophermarti@icloud.com>
2017-08-04 18:18:56 -0400

    first version

---
+++ dumb.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import webbrowser
+
+request = requests.get('https://dog.ceo/dog-api/')
+response = requests.post('https://dog.ceo/api/breeds/list')
+
+doggo = input("Doggo: ")
+requested_doggo = json.loads(requests.post('https://dog.ceo/api/breeds/list').text)
+
+if doggo in requested_doggo['message']:
+    requested_images = requests.post('https://dog.ceo/api/breed/{0}/images'.format(doggo))
+    image_results = json.loads(requested_images.text)
+    print(request.url)
+    print(response)
+    print ("incoming ", doggo, " pics!\ngood boy! bork!")
+    print (image_results)
+    for doge in image_results['message']:
+        webbrowser.open(doge)
+else:
+    print("ruh roh! we didn't find any ", doggo, "pics!")
