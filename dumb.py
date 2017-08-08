cabf5c5e5656ee522072fa30ebf3e4a01d21124e
christopher <christophermarti@users.noreply.github.com>
2017-08-08 17:13:54 -0400

    overengineering boi
    WOOF

--- dumb.py
+++ dumb.py
@@ -1,23 +1,37 @@
 import requests
 import json
 import webbrowser
+import sys
 
 request = requests.get('https://dog.ceo/dog-api/')
+print(request.url)
 response = requests.post('https://dog.ceo/api/breeds/list')
+print(response)
 
-doggo = input("Doggo: ")
-requested_doggo = json.loads(requests.post('https://dog.ceo/api/breeds/list').text)
+print("Bork! Welcome to the Doggo Database!")
+print("What would you like to do?")
+print("*press [1] to see a list of doggos!")
+print("*press [2] to search for pics of a specific good boy!")
+print("*press [3] to quit! WOOF!")
 
-if doggo in requested_doggo['message']:
-    requested_images = requests.post('https://dog.ceo/api/breed/{0}/images'.format(doggo))
-    image_results = json.loads(requested_images.text)
-    print(request.url)
-    print(response)
-    print (image_results)
-    print ("incoming ", doggo, " pics!\ngood boy! bork!")
-    from time import sleep
-    sleep(5)
-    for doge in image_results['message']:
-        webbrowser.open(doge)
-else:
-    print("ruh roh! we didn't find any ", doggo, "pics!")
+def pics(doggo): 
+    requested_doggo = json.loads(requests.post('https://dog.ceo/api/breeds/list').text)
+    if doggo in requested_doggo['message']:
+        requested_images = requests.post('https://dog.ceo/api/breed/{0}/images'.format(doggo))
+        image_results = json.loads(requested_images.text)
+        print (image_results)
+        print (len(image_results['message']),"incoming ", doggo, " pics!\ngood boy! bork!")
+        from time import sleep
+        sleep(5)
+        for doge in image_results['message']:
+            webbrowser.open(doge)
+    else:
+        print("ruh roh! we didn't find any ", doggo, "pics!")
+
+def menu(choice):
+    if choice == "1":
+        print(json.loads(requests.post('https://dog.ceo/api/breeds/list/all').text))
+    else: 
+        pics(doggo = input("Doggo: "))
+
+menu(choice = input("Choice: "))
