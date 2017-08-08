11928222d81ace18838969568eadd382681013ca
christopher <christophermarti@users.noreply.github.com>
2017-08-08 17:20:57 -0400

    OD sadly
    what have I done

--- dumb.py
+++ dumb.py
@@ -2,17 +2,19 @@ import requests
 import json
 import webbrowser
 import sys
+from time import sleep
 
 request = requests.get('https://dog.ceo/dog-api/')
 print(request.url)
 response = requests.post('https://dog.ceo/api/breeds/list')
 print(response)
 
-print("Bork! Welcome to the Doggo Database!")
-print("What would you like to do?")
-print("*press [1] to see a list of doggos!")
-print("*press [2] to search for pics of a specific good boy!")
-print("*press [3] to quit! WOOF!")
+def greeting(): 
+    print("Bork! Welcome to the Doggo Database!")
+    print("What would you like to do?")
+    print("*press [1] to see a list of doggos!")
+    print("*press [2] to search for pics of a specific good boy!")
+    print("*press [3] to quit! WOOF!")
 
 def pics(doggo): 
     requested_doggo = json.loads(requests.post('https://dog.ceo/api/breeds/list').text)
@@ -21,7 +23,6 @@ def pics(doggo):
         image_results = json.loads(requested_images.text)
         print (image_results)
         print (len(image_results['message']),"incoming ", doggo, " pics!\ngood boy! bork!")
-        from time import sleep
         sleep(5)
         for doge in image_results['message']:
             webbrowser.open(doge)
@@ -31,7 +32,39 @@ def pics(doggo):
 def menu(choice):
     if choice == "1":
         print(json.loads(requests.post('https://dog.ceo/api/breeds/list/all').text))
-    else: 
+    elif choice == "2": 
         pics(doggo = input("Doggo: "))
+    else:
+        print("GOODBYE!")
+        sad_doge="""\
+                                      ,.  , 
+                          .-. \ \| \ 
+             ,---._    _,-.> `.\ \ ( 
+            (__,'  `   `>-         -\ 
+                     ,-'             `-. 
+         ,-'       ,  ,    .       .    `. 
+       ,'\       ,' ,-'    `-.      ;    :`. 
+      (__;     ,',,'      ,   `     : `. :  \ 
+             ,' |  _,'   /_    `    :  ; :   \ 
+            /  ,',' |   /  \        '     ;   \ 
+           /   | |(o|  /  (o)          |  |    ; 
+          /     ___-^-^-----.          |  |    | 
+         (   ,---. `-.           :.    |       : 
+          ;,'      )  `          :..   |        | 
+          :\      /              :.    |        ; 
+          :.`-.__,              ,:`    |        | 
+          ;`.    .             ':'      \      , 
+         /   `-.__\           '      ,   \     \. 
+        (   ,'    \`--,-----.       /     \     \`. 
+         `-'       \,' ,'   /    / |       \     | `. 
+      -hrr-        /  '   ,'    /-.|        `.   ;   `. 
+                  (      /`----'   |          `--'     ` 
+                   `.__,' 
+                          
+        """
+        print(sad_doge)
+        sleep(0.75)
+        sys.exit
 
+greeting()
 menu(choice = input("Choice: "))
